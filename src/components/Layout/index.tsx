7cc14254567fb528b49e7720929e178708e6268e
niel <niel.heo@gmail.com>
2018-02-03 22:29:51 +0700

    Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,9 +3,13 @@ import { WithStyles, withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import Hidden from 'material-ui/Hidden';
 
 export interface LayoutProps {
     children?: React.ReactNode;
@@ -22,15 +26,49 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  body: {
+    flexGrow: 1,
+    margin: 12,
+  },
+  menu: {
+    textAlign: 'left',
+    padding: 12,
+  },
+  content: {
+    padding: 12,
+    textAlign: 'left',
+  },
 };
 
-type PropsWithStyles = LayoutProps & WithStyles<'root' | 'flex' | 'menuButton'>;
+type PropsWithStyles = LayoutProps & WithStyles<'root' | 'flex' | 'menuButton' | 'body' | 'menu' | 'content' >;
 
-class Layout extends React.Component<PropsWithStyles, {}> {
+class Layout extends React.Component<PropsWithStyles, 
+  {auth: boolean, 
+    anchorEl: EventTarget & HTMLElement | undefined}> {
+  constructor(props: PropsWithStyles) {
+    super(props);
+    this.state = {
+      auth: true,
+      anchorEl: undefined,
+    };
+  }
+
+  handleChange = (event: Function, checked: boolean) => {
+    this.setState({ auth: checked });
+  }
+
+  handleMenu = (event: MouseEvent<HTMLElement>) => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: undefined });
+  }
+
   public render() {
-    console.log('Layout');
-    console.log(this.props);
     const {classes} = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
     return (
       <section>
     <div className={classes.root}>
@@ -40,23 +78,55 @@ class Layout extends React.Component<PropsWithStyles, {}> {
             <MenuIcon />
           </IconButton>
           <Typography type="title" color="inherit" className={classes.flex}>
-            Title
+            TravelConn
           </Typography>
-          <Button color="inherit">Login</Button>
+          {auth && (
+          <div>
+            <IconButton
+              aria-owns={open ? 'menu-appbar' : undefined}
+              aria-haspopup="true"
+              onClick={this.handleMenu}
+              color="inherit"
+              
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+              <MenuItem onClick={this.handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-sm-3">
-            Otak otak
-        </div>
-        <div className="col-sm-9">
-          {this.props.children}
-        </div>
-      </div>
+    <div className={classes.body}>
+      <Grid container={true}>
+        <Hidden smDown={true}>
+          <Grid item={true} md={3} lg={2} xl={2}>
+            <Paper className={classes.menu}>Menu</Paper>
+          </Grid> 
+        </Hidden>
+        <Grid item={true} sm={12} md={9} lg={10} xl={10}>
+          <Paper className={classes.content}>{this.props.children}</Paper>
+        </Grid>  
+      </Grid>
     </div></section>);
   } 
 }
 
 export default withStyles(styles)(Layout);
+import { MouseEvent } from 'react';
